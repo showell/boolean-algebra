30dc4607ceb0805bf6b1f5a95961a745e1df930d
root <showell30@yahoo.com>
2023-03-27 15:47:21 +0000

    Use OrClause as degenerate case for single var.

--- bool.py
+++ bool.py
@@ -18,11 +18,8 @@ class Expression:
         if type(other) == FalseVal:
             return FALSE
 
-        if type(other) == Var:
-            return self.AND_VAR(other).simplify()
-
-        if type(other) == NegatedVar:
-            return self.AND_NEGATED_VAR(other).simplify()
+        if type(other) == OrClause:
+            return self.AND_OR_CLAUSE(other).simplify()
 
         assert False
 
@@ -33,30 +30,12 @@ class Expression:
         if type(other) == FalseVal:
             return self
 
-        if type(other) == Var:
-            return self.OR_VAR(other).simplify()
-
-        if type(other) == NegatedVar:
-            return self.OR_NEGATED_VAR(other).simplify()
-
         if type(other) == OrClause:
             return self.OR_OR_CLAUSE(other).simplify()
 
         assert False
 
     def NOT(self):
-        assert False
-
-    def AND_VAR(self, other):
-        assert False
-
-    def OR_VAR(self, other):
-        assert False
-
-    def AND_NEGATED_VAR(self, other):
-        assert False
-
-    def OR_NEGATED_VAR(self, other):
         assert False
 
     def OR_OR_CLAUSE(self, other):
@@ -91,73 +70,6 @@ class FalseVal(Expression):
         return TRUE
 
 
-class Var(Expression):
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def AND_VAR(self, other):
-        assert other.name == self.name
-        return self
-
-    def OR_VAR(self, other):
-        if other.name == self.name:
-            return self
-        else:
-            return OrClause([self.name, other.name], [])
-
-    def AND_NEGATED_VAR(self, other):
-        assert other.name == self.name
-        return FALSE
-
-    def OR_NEGATED_VAR(self, other):
-        if other.name == self.name:
-            return TRUE
-        else:
-            return OrClause([self.name], [other.name])
-
-    def OR_OR_CLAUSE(self, other):
-        return other.OR_VAR(self)
-
-    def NOT(self):
-        return NegatedVar(self.name)
-
-
-class NegatedVar(Expression):
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return "~" + self.name
-
-    def AND_VAR(self, other):
-        assert other.name == self.name
-        return FALSE
-
-    def OR_VAR(self, other):
-        if other.name == self.name:
-            return TRUE
-        else:
-            return OrClause([other.name], [self.name])
-
-    def AND_NEGATED_VAR(self, other):
-        assert other.name == self.name
-        return self
-
-    def OR_NEGATED_VAR(self, other):
-        if other.name == self.name:
-            return self
-        return OrClause([], [self.name, other.name])
-
-    def OR_OR_CLAUSE(self, other):
-        return other.OR_NEGATED_VAR(self)
-
-    def NOT(self):
-        return Var(self.name)
-
-
 class OrClause(Expression):
     def __init__(self, names, negated_names):
         self.names = sorted(names)
@@ -168,24 +80,21 @@ class OrClause(Expression):
         neg = ["~" + name for name in self.negated_names]
         return "|".join(pos + neg)
 
-    def AND_VAR(self, other):
-        if other.name in self.names:
-            return other
-        assert False
-
-    def OR_VAR(self, other):
-        if other.name in self.names:
+    def simplify(self):
+        names = list(sorted(self.names))
+        negated_names = list(sorted(self.negated_names))
+        if names == self.names and negated_names == self:
             return self
-        if other.name in self.negated_names:
-            return TRUE
-        return OrClause(self.names + [other.name], self.negated_names)
 
-    def OR_NEGATED_VAR(self, other):
-        if other.name in self.names:
-            return TRUE
-        if other.name in self.negated_names:
-            return self
-        return OrClause(self.names, self.negated_names + [other.name])
+        if names or negated_names:
+            return OrClause(names, negated_names)
+
+        return TRUE
+
+    def tups(self):
+        return [(True, name) for name in self.names] + [
+            (False, name) for name in self.negated_names
+        ]
 
     def OR_OR_CLAUSE(self, other):
         names = set(self.names) | set(other.names)
@@ -193,6 +102,29 @@ class OrClause(Expression):
         if names & negated_names:
             return TRUE
         return OrClause(names, negated_names)
+
+    def AND_OR_CLAUSE(self, other):
+        names = []
+        negated_names = []
+        for self_sign, self_name in self.tups():
+            for other_sign, other_name in other.tups():
+                assert self_name == other_name
+                if self_name == other_name:
+                    if self_sign != other_sign:
+                        return FALSE
+                    if self_sign:
+                        names.append(self_name)
+                    else:
+                        negated_names.append(self_name)
+
+        return OrClause(names, negated_names)
+
+    def NOT(self):
+        if len(self.names) == 1 and len(self.negated_names) == 0:
+            return OrClause([], self.names)
+        if len(self.names) == 0 and len(self.negated_names) == 1:
+            return OrClause(self.negated_names, [])
+        assert False
 
 
 TRUE = TrueVal()
@@ -226,4 +158,4 @@ def OR(*lst):
 
 
 def SYMBOL(name):
-    return Var(name)
+    return OrClause([name], [])
