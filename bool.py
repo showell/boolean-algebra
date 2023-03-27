c43bbc2528e9f6948be6eca3aa02dab515fc3a7c
root <showell30@yahoo.com>
2023-03-27 13:35:35 +0000

    single-var logic mostly done

--- bool.py
+++ bool.py
@@ -1,31 +1,13 @@
-class TrueVal:
-    def __str__(self):
-        return "T"
-    
-    def AND(self, other):
-        return other
-
-    def OR(self, other):
-        return TRUE
-
-    def NOT(self):
-        return FALSE
-
-
-class FalseVal:
-    def __str__(self):
-        return "F"
-    
-    def AND(self, other):
-        return FALSE
-
-    def OR(self, other):
-        return other
-
-    def NOT(self):
-        return TRUE
-
 class Expression:
+    def __and__(self, other):
+        return self.AND(other)
+
+    def __or__(self, other):
+        return self.OR(other)
+
+    def __invert__(self):
+        return self.NOT()
+
     def AND(self, other):
         if type(other) == TrueVal:
             return self
@@ -35,6 +17,9 @@ class Expression:
 
         if type(other) == Var:
             return self.AND_VAR(other)
+
+        if type(other) == NegatedVar:
+            return self.AND_NEGATED_VAR(other)
 
         assert False
 
@@ -48,13 +33,51 @@ class Expression:
         if type(other) == Var:
             return self.OR_VAR(other)
 
+        if type(other) == NegatedVar:
+            return self.OR_NEGATED_VAR(other)
+
         assert False
 
-    def AND_VAR(self):
+    def AND_VAR(self, other):
         assert False
 
-    def OR_VAR(self):
+    def OR_VAR(self, other):
         assert False
+
+    def AND_NEGATED_VAR(self, other):
+        assert False
+
+    def OR_NEGATED_VAR(self, other):
+        assert False
+
+
+class TrueVal(Expression):
+    def __str__(self):
+        return "T"
+
+    def AND(self, other):
+        return other
+
+    def OR(self, other):
+        return TRUE
+
+    def NOT(self):
+        return FALSE
+
+
+class FalseVal(Expression):
+    def __str__(self):
+        return "F"
+
+    def AND(self, other):
+        return FALSE
+
+    def OR(self, other):
+        return other
+
+    def NOT(self):
+        return TRUE
+
 
 class Var(Expression):
     def __init__(self, name):
@@ -71,29 +94,74 @@ class Var(Expression):
         assert other.name == self.name
         return self
 
+    def AND_NEGATED_VAR(self, other):
+        assert other.name == self.name
+        return FALSE
+
+    def OR_NEGATED_VAR(self, other):
+        assert other.name == self.name
+        return TRUE
+
+    def NOT(self):
+        return NegatedVar(self.name)
+
+
+class NegatedVar(Expression):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "~" + self.name
+
+    def AND_VAR(self, other):
+        assert other.name == self.name
+        return FALSE
+
+    def OR_VAR(self, other):
+        assert other.name == self.name
+        return TRUE
+
+    def AND_NEGATED_VAR(self, other):
+        assert other.name == self.name
+        return self
+
+    def OR_NEGATED_VAR(self, other):
+        assert other.name == self.name
+        return self
+
+    def NOT(self):
+        return Var(self.name)
+
+
 TRUE = TrueVal()
 FALSE = FalseVal()
+
 
 def _AND2(a, b):
     return a.AND(b)
 
+
 def _OR2(a, b):
     return a.OR(b)
+
 
 def NOT(a):
     return a.NOT()
 
+
 def AND(*lst):
     assert len(lst) >= 1
     if len(lst) == 1:
-        return lst[0] 
+        return lst[0]
     return _AND2(lst[0], AND(*lst[1:]))
+
 
 def OR(*lst):
     assert len(lst) >= 1
     if len(lst) == 1:
-        return lst[0] 
+        return lst[0]
     return _OR2(lst[0], OR(*lst[1:]))
+
 
 def SYMBOL(name):
     return Var(name)
--- test_simple.py
+++ test_simple.py
@@ -1,10 +1,13 @@
 from bool import TRUE, FALSE, AND, OR, NOT, SYMBOL
 from lib.test_helpers import run_test, assert_str
 
+T = TRUE
+F = FALSE
+x = SYMBOL("x")
+
+
 @run_test
 def simple():
-    T = TRUE
-    F = FALSE 
     assert_str(T, "T")
     assert_str(F, "F")
     assert_str(AND(F, T), "F")
@@ -12,7 +15,6 @@ def simple():
     assert_str(AND(T, T, F), "F")
     assert_str(OR(F, T, T), "T")
 
-    x = SYMBOL("x")
     assert_str(x, "x")
     assert_str(AND(x, x), "x")
     assert_str(OR(x, x), "x")
@@ -22,3 +24,47 @@ def simple():
 
     assert_str(AND(x, F), "F")
     assert_str(OR(x, F), "x")
+
+
+@run_test
+def negated_vars():
+    assert_str(~x, "~x")
+
+    assert_str(~x & ~x, "~x")
+    assert_str(~x | ~x, "~x")
+
+
+@run_test
+def double_negation():
+    assert_str(~~x, "x")
+    assert_str(~~~~x, "x")
+
+    assert_str(~~~x, "~x")
+    assert_str(~~~~~x, "~x")
+
+
+@run_test
+def identity():
+    assert_str(x & T, "x")
+    assert_str(x | F, "x")
+
+
+@run_test
+def annihilator():
+    assert_str(x & F, "F")
+    assert_str(x | T, "T")
+
+
+@run_test
+def idemptotence():
+    assert_str(x & x, "x")
+    assert_str(x | x, "x")
+
+
+@run_test
+def complementation():
+    assert_str(x & ~x, "F")
+    assert_str(x | ~x, "T")
+
+    assert_str(~x & x, "F")
+    assert_str(~x | x, "T")
