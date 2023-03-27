4aca1cc519274100a84285e448349526708edbb4
root <showell30@yahoo.com>
2023-03-27 23:53:41 +0000

    Move function.

--- bool.py
+++ bool.py
@@ -1,51 +1,3 @@
-def SignedVar_AND_SignedVar(x, y):
-    if x.name == y.name:
-        return x if x.sign == y.sign else FALSE
-    return AndClause([x, y])
-
-def SignedVar_OR_SignedVar(x, y):
-    if x.name == y.name:
-        return x if x.sign == y.sign else TRUE
-    return OrClause([x, y])
-
-def AndClause_AND_AndClause(x, y):
-    names = x.names | y.names
-    negated_names = x.negated_names | y.negated_names
-    if names & negated_names:
-        return FALSE
-    return AndClause.make(names, negated_names)
-
-def OrClause_OR_OrClause(x, y):
-    names = x.names | y.names
-    negated_names = x.negated_names | y.negated_names
-    if names & negated_names:
-        return TRUE
-    return OrClause.make(names, negated_names)
-
-def AndClause_OR_SignedVar(clause, signed_var):
-    for sv in clause.signed_vars:
-        if sv.name == signed_var.name:
-            if sv.sign == signed_var.sign:
-                return signed_var
-            else:
-                break
-    assert False
-
-def OrClause_AND_SignedVar(clause, signed_var):
-    for sv in clause.signed_vars:
-        if sv.name == signed_var.name:
-            if sv.sign == signed_var.sign:
-                return signed_var
-            else:
-                break
-    assert False
-
-def OrClause_OR_SignedVar(clause, signed_var):
-    return OrClause_OR_OrClause(clause, OrClause([signed_var]))
-
-def AndClause_AND_SignedVar(clause, signed_var):
-    return AndClause_AND_AndClause(clause, AndClause([signed_var]))
-
 class Expression:
     def __and__(self, other):
         return self.AND(other)
@@ -230,3 +182,52 @@ FALSE = FalseVal()
 
 def SYMBOL(name):
     return SignedVar(name, True)
+
+def SignedVar_AND_SignedVar(x, y):
+    if x.name == y.name:
+        return x if x.sign == y.sign else FALSE
+    return AndClause([x, y])
+
+def SignedVar_OR_SignedVar(x, y):
+    if x.name == y.name:
+        return x if x.sign == y.sign else TRUE
+    return OrClause([x, y])
+
+def AndClause_AND_AndClause(x, y):
+    names = x.names | y.names
+    negated_names = x.negated_names | y.negated_names
+    if names & negated_names:
+        return FALSE
+    return AndClause.make(names, negated_names)
+
+def OrClause_OR_OrClause(x, y):
+    names = x.names | y.names
+    negated_names = x.negated_names | y.negated_names
+    if names & negated_names:
+        return TRUE
+    return OrClause.make(names, negated_names)
+
+def AndClause_OR_SignedVar(clause, signed_var):
+    for sv in clause.signed_vars:
+        if sv.name == signed_var.name:
+            if sv.sign == signed_var.sign:
+                return signed_var
+            else:
+                break
+    assert False
+
+def OrClause_AND_SignedVar(clause, signed_var):
+    for sv in clause.signed_vars:
+        if sv.name == signed_var.name:
+            if sv.sign == signed_var.sign:
+                return signed_var
+            else:
+                break
+    assert False
+
+def OrClause_OR_SignedVar(clause, signed_var):
+    return OrClause_OR_OrClause(clause, OrClause([signed_var]))
+
+def AndClause_AND_SignedVar(clause, signed_var):
+    return AndClause_AND_AndClause(clause, AndClause([signed_var]))
+
