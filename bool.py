9837e2ddf7416a13be0cda5ae5923f7562e6bab5
root <showell30@yahoo.com>
2023-03-27 13:05:13 +0000

    var

--- bool.py
+++ bool.py
@@ -25,6 +25,52 @@ class FalseVal:
     def NOT(self):
         return TRUE
 
+class Expression:
+    def AND(self, other):
+        if type(other) == TrueVal:
+            return self
+
+        if type(other) == FalseVal:
+            return FALSE
+
+        if type(other) == Var:
+            return self.AND_VAR(other)
+
+        assert False
+
+    def OR(self, other):
+        if type(other) == TrueVal:
+            return TRUE
+
+        if type(other) == FalseVal:
+            return self
+
+        if type(other) == Var:
+            return self.OR_VAR(other)
+
+        assert False
+
+    def AND_VAR(self):
+        assert False
+
+    def OR_VAR(self):
+        assert False
+
+class Var(Expression):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def AND_VAR(self, other):
+        assert other.name == self.name
+        return self
+
+    def OR_VAR(self, other):
+        assert other.name == self.name
+        return self
+
 TRUE = TrueVal()
 FALSE = FalseVal()
 
@@ -48,3 +94,6 @@ def OR(*lst):
     if len(lst) == 1:
         return lst[0] 
     return _OR2(lst[0], OR(*lst[1:]))
+
+def SYMBOL(name):
+    return Var(name)
--- test_simple.py
+++ test_simple.py
@@ -1,4 +1,4 @@
-from bool import TRUE, FALSE, AND, OR, NOT
+from bool import TRUE, FALSE, AND, OR, NOT, SYMBOL
 from lib.test_helpers import run_test, assert_str
 
 @run_test
@@ -12,4 +12,13 @@ def simple():
     assert_str(AND(T, T, F), "F")
     assert_str(OR(F, T, T), "T")
 
+    x = SYMBOL("x")
+    assert_str(x, "x")
+    assert_str(AND(x, x), "x")
+    assert_str(OR(x, x), "x")
 
+    assert_str(AND(x, T), "x")
+    assert_str(OR(x, T), "T")
+
+    assert_str(AND(x, F), "F")
+    assert_str(OR(x, F), "x")
